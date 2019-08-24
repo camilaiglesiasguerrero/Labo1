3d76025c8bc00047d068b726d77c43a4d24e4e1e
camilaiglesiasguerrero <camila.ig08@gmail.com>
2019-08-24 18:36:21 -0300

    Sumo en una función e informo si pude hacer el cálculo solicitaod de manera correcta. Pasaje por referencia

--- Clase1/src/clase1.c
+++ Clase1/src/clase1.c
@@ -11,9 +11,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int sumar(int num1, int num2);
+int sumar(int num1, int num2, int* resultado);
 
-//Le pido al usuario el ingreso de dos números, los sumo en una función e informo el resultado desde el main
+/**
+ * Le pido al usuario el ingreso de dos números. Si ambos son positivos, los sumo en una función
+ * e informo que pude realizar el cálculo correctamente y el resultado
+ * */
 int main(void) {
 	int num1;
 	int num2;
@@ -25,16 +28,31 @@ int main(void) {
 	printf("Ingrese otro numero: ");
 	fflush(stdout);
 	scanf("%d",&num2);
-	resultado = sumar(num1, num2);
-	printf("Numero 1: %d - Numero 2: %d \n%d + %d = %d",num1,num2,num1,num2,resultado);
+	if(sumar(num1, num2, &resultado) == 0)
+	{
+		printf("Ambos números son positivos\n");
+		printf("Numero 1: %d - Numero 2: %d \n%d + %d = %d",num1,num2,num1,num2,resultado);
+	}
+	else
+	{
+		printf("Error. Ambos números deben ser positivos");
+	}
 
 	return 0;
 }
 
-int sumar(int num1, int num2)
+int sumar(int num1, int num2, int* resultado)
 {
-	int resultado;
-	resultado = num1 + num2;
+	int retorno;
+	if(num1 > 0 && num2 > 0)
+	{
+		*resultado = num1 + num2;
+		retorno = 0;
+	}
+	else
+	{
+		retorno = -1;
+	}
 
-	return resultado;
+	return retorno;
 }
